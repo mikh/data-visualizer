df447c70801b05c7efe6ca28566e56e13bdf7f92
Mikhail Andreev <mikha.andreev@gmail.com>
2025-12-12 11:57:33 -0500

    Adding a filestats model for getting stats of data

--- flask/db/models.py
+++ flask/db/models.py
@@ -2,7 +2,7 @@
 
 from typing import List, Dict, Any
 
-from sqlalchemy import String, Table, Column, Integer, ForeignKey
+from sqlalchemy import String, Table, Column, Integer, ForeignKey, Float
 from sqlalchemy.orm import Mapped, mapped_column, declarative_base, relationship
 
 Base = declarative_base()
@@ -12,6 +12,13 @@ file_tags = Table(
     Base.metadata,
     Column("file_id", Integer, ForeignKey("file_metadata.id"), primary_key=True),
     Column("tag_id", Integer, ForeignKey("tag.id"), primary_key=True),
+)
+
+file_column_stats = Table(
+    "file_column_stats",
+    Base.metadata,
+    Column("file_stats_id", Integer, ForeignKey("file_stats.id"), primary_key=True),
+    Column("column_stats_id", Integer, ForeignKey("column_stats.id"), primary_key=True),
 )
 
 
@@ -29,6 +36,11 @@ class FileMetadata(Base):  # pylint: disable=too-few-public-methods
     tags: Mapped[List["Tag"]] = relationship(
         "Tag",
         secondary="file_tags",
+        back_populates="file_metadata",
+    )
+
+    file_stats: Mapped["FileStats"] = relationship(
+        "FileStats",
         back_populates="file_metadata",
     )
 
@@ -68,3 +80,52 @@ class Tag(Base):  # pylint: disable=too-few-public-methods
             "id": self.id,
             "name": self.name,
         }
+
+
+class FileStats(Base):  # pylint: disable=too-few-public-methods
+    """File Stats Model.
+    This model is used to store the statistics for a given file.
+    """
+
+    __tablename__ = "file_stats"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    num_columns: Mapped[int] = mapped_column(Integer, nullable=False)
+    num_rows: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    column_stats: Mapped[List["ColumnStats"]] = relationship(
+        "ColumnStats",
+        secondary="file_column_stats",
+        back_populates="file_stats",
+    )
+
+
+class ColumnStats(Base):  # pylint: disable=too-few-public-methods
+    """Column stats model."""
+
+    __tablename__ = "column_stats"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    column_name: Mapped[str] = mapped_column(String, nullable=False)
+    data_type: Mapped[str] = mapped_column(String, nullable=False)
+    num_rows: Mapped[int] = mapped_column(Integer, nullable=False)
+    num_unique_values: Mapped[int] = mapped_column(Integer, nullable=False)
+    num_null_values: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    # Numeric stats
+    num_zeros_values: Mapped[int] = mapped_column(Integer, nullable=False)
+    std_dev: Mapped[float] = mapped_column(Float, nullable=False)
+    mean: Mapped[float] = mapped_column(Float, nullable=False)
+    median: Mapped[float] = mapped_column(Float, nullable=False)
+    min_value: Mapped[float] = mapped_column(Float, nullable=False)
+    max_value: Mapped[float] = mapped_column(Float, nullable=False)
+
+    # Categorical stats
+    num_empty_values: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    file_stats: Mapped["FileStats"] = relationship(
+        "FileStats",
+        secondary="file_column_stats",
+        back_populates="column_stats",
+    )
