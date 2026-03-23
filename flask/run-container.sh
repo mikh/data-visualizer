fd3a705b6efc046ae5b7aaefcd5fdcf2d112e5fb
Mikhail Andreev <mikha.andreev@gmail.com>
2026-03-23 14:10:01 -0400

    Fix bug in data-file storage

--- flask/run-container.sh
+++ flask/run-container.sh
@@ -4,18 +4,18 @@ set -eo pipefail
 
 cd /app
 
-export FILE_BASE_DIR="/data/files"
+export DATA_FILE_DIR="/data/files"
 export LOG_DIR="/data/logs"
 export DB_PATH="/data/metadata.sqlite"
 export VERSION_FILE="/app/version"
 
-mkdir -p $FILE_BASE_DIR
+mkdir -p $DATA_FILE_DIR
 mkdir -p $LOG_DIR
 
 if [ ! -f $DB_PATH ]; then
     python -m db.db_control \
         --db-path $DB_PATH \
-        --data-file-dir $FILE_BASE_DIR \
+        --data-file-dir $DATA_FILE_DIR \
         create
 fi
 
